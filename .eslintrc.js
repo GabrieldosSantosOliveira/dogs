afba45261c6ec978b8c942182a8020e32a036fc1
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-06-25 23:28:52 -0400

    chore: add config @rocketseat/eslint-config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,37 +4,6 @@ module.exports = {
     node: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime',
-    'prettier',
+    "@rocketseat/eslint-config"
   ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'eslint-plugin-import-helpers',
-    'prettier',
-    'react-native',
-  ],
-  rules: {
-    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'all' }],
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        newlinesBetween: 'always',
-        groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
-        alphabetize: {
-          order: 'asc',
-          ignoreCase: true,
-        },
-      },
-    ],
-  },
 };
--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
